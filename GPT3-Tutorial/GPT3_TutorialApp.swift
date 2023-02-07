9de64447159144745949efff69e138fa67ce9990
troglodisme <giulio.ammendola@network.rca.ac.uk>
2023-02-07 10:25:30 +0000

    Initial Commit

---
+++ GPT3-Tutorial/GPT3_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  GPT3_TutorialApp.swift
+//  GPT3-Tutorial
+//
+//  Created by Giulio on 07/02/23.
+//
+
+import SwiftUI
+
+@main
+struct GPT3_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
